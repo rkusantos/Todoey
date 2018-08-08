8b57f423e4817045a0678f4d9248d8d2a1dd4f0b
Ronn Santos <ronn_the_great12324@yahoo.com>
2018-08-08 10:03:48 +0400

    sandbox

--- Todoey/TodoListViewContrroller.swift
+++ Todoey/TodoListViewContrroller.swift
@@ -12,9 +12,15 @@ class TodoListViewController: UITableViewController {
     
     var itemArray = ["Like Mike", "Mamba", "LBJ"]
     
+    
+    let defaults = UserDefaults.standard
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        if let items = defaults.array(forKey: "TodoListArray") as! [String] {
+            itemArray = items
+        }
         
         
     }
@@ -68,6 +74,9 @@ class TodoListViewController: UITableViewController {
         
             //what will happen once the user clicks the Add Item button on our UIAlert
             self.itemArray.append(textField.text!)
+            
+            self.defaults.set(self.itemArray, forKey: "TodoListArray")
+            
             self.tableView.reloadData()
         }
         alert.addTextField { (alertTextField) in
