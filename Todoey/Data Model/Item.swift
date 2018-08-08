31ca69c772572e8f81975b5cd24ea6ce8380eef8
Ronn Santos <ronn_the_great12324@yahoo.com>
2018-08-08 12:16:53 +0400

    MVC Architecure

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Ronn Kevin Santos on 8/8/18.
+//  Copyright © 2018 Catch. All rights reserved.
+//
+
+import UIKit
+
+class Item {
+
+    var title : String = ""
+    
+    var done : Bool = false
+    
+    
+
+}
